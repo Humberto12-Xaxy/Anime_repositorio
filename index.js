0247c661528abf3052aac925cfcadf9a850908aa
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2022-05-30 20:16:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const jikan = require('jikan4.js');
+
+app.use(express.json());
+
+const client = new jikan.Client()
+
+app.get('/anime', async (req, res) => {
+    let {id} = req.body
+    const anime = await client.anime.get(id)
+    
+    if(anime){
+        res.json({id : anime.id, nombre : anime.title.default, synopsis: anime.synopsis})
+    }else{
+        res.json({error: 'El ID no existe'})
+    }
+});
+
+app.get('/animep/:id', async (req, res) =>{
+    let {id} = req.params
+    const anime = await client.anime.get(id)
+    
+    if(anime){
+        res.json({id : anime.id, nombre : anime.title.default, synopsis: anime.synopsis})
+    }else{
+        res.json({error: 'El ID no existe'})
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+});
