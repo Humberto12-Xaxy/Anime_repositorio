fbdb4a5a9d0022b8d5bb68dd8307643a3bfa35c0
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2022-06-29 11:20:46 -0500

    prueba 2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const client = new jikan.Client()
 
 app.get('/', (req, res)=>{
-    res.send('Chtm frozy')
+    res.send('Chtm Gabi')
 })
 
 app.get('/anime', async (req, res) => {
