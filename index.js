977fc72a5dd1366a4d17e881b1ac84320608f23e
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2022-06-30 09:09:31 -0500

    Ya me aburriiiii de probar x3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const client = new jikan.Client()
 
 app.get('/', (req, res)=>{
-    res.send('Prueba pipeline chtm frozy')
+    res.send('Prueba pipeline chtm frozy x2')
 })
 
 app.get('/anime', async (req, res) => {
