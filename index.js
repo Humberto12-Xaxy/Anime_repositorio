28a21609d75bdad5214bfe08b848fd62c52bf967
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2022-06-29 21:07:47 -0500

    ya me aburri de probar

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const client = new jikan.Client()
 
 app.get('/', (req, res)=>{
-    res.send('Hello gabi')
+    res.send('Prueba pipeline chtm frozy')
 })
 
 app.get('/anime', async (req, res) => {
